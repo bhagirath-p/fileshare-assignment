320b88153597b5bc8c062d0d86af65892a0fdb18
Bhagirath Purushothama <bhagirath.p@hotmail.com>
2025-12-01 13:41:43 +0000

    Fixing the error.

--- api_gateway.tf
+++ api_gateway.tf
@@ -27,13 +27,13 @@ resource "aws_api_gateway_resource" "presign" {
   path_part   = "presign"
 }
 
-resource "aws_api_gateway_method" "presign_method" {
-  rest_api_id   = aws_api_gateway_rest_api.file_api.id
-  resource_id   = aws_api_gateway_resource.presign.id
-  http_method   = "POST"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.cognito.id
-}
+# resource "aws_api_gateway_method" "presign_method" {
+#   rest_api_id   = aws_api_gateway_rest_api.file_api.id
+#   resource_id   = aws_api_gateway_resource.presign.id
+#   http_method   = "POST"
+#   authorization = "COGNITO_USER_POOLS"
+#   authorizer_id = aws_api_gateway_authorizer.cognito.id
+# }
 
 resource "aws_api_gateway_method" "post_presign" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
